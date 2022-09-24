20bfa76016acd087f7db22193d8094f614e2d5dc
qpakr38 <zczc0819@gmail.com>
2022-09-24 19:48:56 +0900

    FEAT : Layout 적용하기

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import { Fragment } from "react";
+import MainHeader from "../components/layout/main-header";
+import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
