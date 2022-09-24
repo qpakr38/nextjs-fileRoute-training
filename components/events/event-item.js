856a4c2e78af8d5fb8de5f6936c00256e66d0404
qpakr38 <zczc0819@gmail.com>
2022-09-24 19:22:18 +0900

    FEAT : Icon 적용

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
   const { title, image, date, id, location } = props.item;
@@ -19,14 +22,21 @@ const EventItem = (props) => {
         <div>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
